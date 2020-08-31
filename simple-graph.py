ef6a3c591909dc09729a5e751a3631f17520c8b4
Saul Leon <saulleon137@gmail.com>
2020-08-31 06:51:50 -0500

    added legends

--- simple-graph.py
+++ simple-graph.py
@@ -2,10 +2,14 @@ import matplotlib.pyplot as plt
 
 x = [1, 2, 3]
 y = [5, 7, 9]
-z = [2, 3, 4]
 
-plt.plot(z, y)
+x2 = [1, 2, 3]
+y2 = [10, 14, 12]
+
+plt.plot(x, y, label="Firt line")
+plt.plot(x2, y2, label="Second line")
 plt.xlabel("x-axis")
 plt.ylabel("important var")
-
+plt.title("Nice title \n Salu2 Cordiales")
+plt.legend()
 plt.show()

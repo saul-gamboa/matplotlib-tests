b8f7279292a7dd738d7faddaa16cdb74f68a1954
Saul Leon <saulleon137@gmail.com>
2020-09-11 23:02:30 -0500

    Gráfica simple

--- simple-graph.py
+++ simple-graph.py
@@ -6,10 +6,10 @@ y = [5, 7, 9]
 x2 = [1, 2, 3]
 y2 = [10, 14, 12]
 
-plt.plot(x, y, label="Firt line")
-plt.plot(x2, y2, label="Second line")
-plt.xlabel("x-axis")
-plt.ylabel("important var")
-plt.title("Nice title \n Salu2 Cordiales")
+plt.plot(x, y, label="Linea 1")
+plt.plot(x2, y2, label="Linea 2")
+plt.xlabel("Entrada")
+plt.ylabel("Resultado")
+plt.title("Título \n Subtítulo")
 plt.legend()
 plt.show()
